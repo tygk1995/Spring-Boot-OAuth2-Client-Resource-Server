69be1b0d24f7c8779a9146b54568bea1d37397ed
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-07-24 23:24:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-boot-oauth2-client-resource-server
+ Spring Boot OAuth2 Client Resource Server
---
+++ src/main/java/cn/com/xuxiaowei/SpringBootOauth2ClientResourceServerApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2ClientResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauth2ClientResourceServerApplication.class, args);
+    }
+
+}
