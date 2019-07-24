6b8eb6997367cf4df6391ecccb275053eb898763
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-07-25 01:04:54 +0800

    获取 code、获取 Token、检查 Token、刷新 Token

--- src/main/java/cn/com/xuxiaowei/configuration/AuthorizationServerConfigurerAdapterConfiguration.java
+++ src/main/java/cn/com/xuxiaowei/configuration/AuthorizationServerConfigurerAdapterConfiguration.java
@@ -15,36 +15,69 @@
  */
 package cn.com.xuxiaowei.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+
+import javax.sql.DataSource;
 
 /**
  * 授权服务器配置
  *
  * @author xuxiaowei
+ * @see <a href="http://127.0.0.1:8080/oauth/authorize?client_id=5e03fb292edd4e478cd7b4d6fc21518c&redirect_uri=http://127.0.0.1:123&response_type=code&scope=base&state=beff3dfc-bad8-40db-b25f-e5459e3d6ad7">获取 code</a>
+ * @see <a href="http://127.0.0.1:8080/oauth/token?code=qa8P6B&client_id=5e03fb292edd4e478cd7b4d6fc21518c&client_secret=da4ce585e30346d3a876340d49e25a01&redirect_uri=http://127.0.0.1:123&grant_type=authorization_code">获取 Token</a>
+ * @see <a href="http://127.0.0.1:8080/oauth/check_token?token=">检查 Token</a>
+ * @see <a href=" http://127.0.0.1:8080/oauth/token?client_id=5e03fb292edd4e478cd7b4d6fc21518c&client_secret=da4ce585e30346d3a876340d49e25a01&grant_type=refresh_token&refresh_token=">刷新 Token</a>
  * @since 0.0.1
  */
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfigurerAdapterConfiguration extends AuthorizationServerConfigurerAdapter {
 
+    private final DataSource dataSource;
+
+    @Autowired
+    public AuthorizationServerConfigurerAdapterConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        super.configure(security);
+        security.tokenKeyAccess("permitAll()");
+        security.checkTokenAccess("permitAll()");
+        security.allowFormAuthenticationForClients();
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        super.configure(clients);
+        // 查询 Client
+        clients.withClientDetails(new JdbcClientDetailsService(dataSource));
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        super.configure(endpoints);
+        // 获取 Token 可使用 GET、POST
+        endpoints.allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
+
+        // 刷新 Token 时查询用户
+        endpoints.userDetailsService(jdbcDaoImpl());
+    }
+
+    /**
+     * 刷新 Token 时查询用户
+     */
+    public JdbcDaoImpl jdbcDaoImpl() {
+        JdbcDaoImpl jdbcDao = new JdbcDaoImpl();
+        jdbcDao.setDataSource(dataSource);
+        return jdbcDao;
     }
 
 }
