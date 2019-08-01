3caf0c43fa302e98ca668a484900a23c4243684f
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-08-01 21:49:00 +0800

    验证签名密钥

--- src/main/java/cn/com/xuxiaowei/configuration/AuthorizationServerConfigurerAdapterConfiguration.java
+++ src/main/java/cn/com/xuxiaowei/configuration/AuthorizationServerConfigurerAdapterConfiguration.java
@@ -72,7 +72,7 @@ import java.util.UUID;
  * @see <a href="http://127.0.0.1:8080/oauth/authorize?client_id=5e03fb292edd4e478cd7b4d6fc21518c&redirect_uri=http://127.0.0.1:123&response_type=code&scope=snsapi_userinfo&state=beff3dfc-bad8-40db-b25f-e5459e3d6ad7">获取 code</a>
  * @see <a href="http://127.0.0.1:8080/oauth/authorize?client_id=5e03fb292edd4e478cd7b4d6fc21518c&redirect_uri=http://127.0.0.1:123&response_type=token&scope=snsapi_base&state=beff3dfc-bad8-40db-b25f-e5459e3d6ad7">获取 Token（implicit，简化模式）</a>
  * @see AuthorizationEndpoint#authorize(Map, Map, SessionStatus, Principal)
- * @see <a href="http://127.0.0.1:8080/oauth/token?code=evP7tmtrRk7Y5bhewjRCo9iUMzcFyByi&client_id=5e03fb292edd4e478cd7b4d6fc21518c&client_secret=da4ce585e30346d3a876340d49e25a01&redirect_uri=http://127.0.0.1:123&grant_type=authorization_code">获取 Token</a>
+ * @see <a href="http://127.0.0.1:8080/oauth/token?code=bHy53RqZSBurCNUaa34rt5VT2RPQBidB&client_id=5e03fb292edd4e478cd7b4d6fc21518c&client_secret=da4ce585e30346d3a876340d49e25a01&redirect_uri=http://127.0.0.1:123&grant_type=authorization_code">获取 Token</a>
  * @see TokenEndpoint#getAccessToken(Principal, Map)
  * @see <a href="http://127.0.0.1:8080/oauth/check_token?token=">检查 Token</a>
  * @see CheckTokenEndpoint#checkToken(String)
@@ -174,7 +174,12 @@ public class AuthorizationServerConfigurerAdapterConfiguration extends Authoriza
 
         // 加密 Token
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        // 设置JWT签名密钥。
+        // 它可以是简单的MAC密钥或RSA密钥。
+        // RSA密钥应采用OpenSSH格式，由<tt> ssh-keygen </tt>生成。
         jwtAccessTokenConverter.setSigningKey("hgiYUt%^&%hiiuoHIH");
+        // 验证签名密钥。
+        jwtAccessTokenConverter.setVerifierKey("&*(jhhjkhHI4HJ46K");
         endpoints.tokenEnhancer(jwtAccessTokenConverter);
     }
 
